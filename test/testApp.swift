f62d6ec9fb4da50b3047b8966bbb98b315760938
Nurislam-React-Developer <98372q@gmailc.com>
2025-09-10 18:57:37 +0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Nurislam Abdimalicov on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
